e9f7b155309160def996cf6fb0b8d7c3ca385647
Razmik <razmik.manvelyan03@mail.ru>
2025-11-18 00:37:11 +0400

    Contactum

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -329,7 +329,7 @@ export default function HomePage({ cardId = "101" }) {
               color: "#444",
             },
           },
-          "Digital Visit Card"
+          "KHContactum Digital Card"
         )
       )
     );
